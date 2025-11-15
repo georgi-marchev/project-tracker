0264c557519be126fac5ed152831b2664e14f9be
georgi-marchev <george_85_bg@yahoo.com>
2025-11-15 11:33:47 +0200

    Create a base app layout that other templates can extend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home - Project Tracker</title>
-</head>
+@section('content')
 
-<body>
-    <h1>Welcome to Project Tracker</h1>
-    <h2>TODO</h2>
-</body>
-
-</html>
+<h1>Welcome to Project Tracker</h1>
+<h2>TODO</h2>
